09e77c66dc7707563f2678da1edba7b7ea1d746c
hermann wenninger <info@wennhmann.de>
2023-07-23 20:10:27 +0200

    caching and more

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -14,15 +14,18 @@ const App = () => {
   const friends = [
     { name: 'Peter', age: 4 },
     { name: 'Maya', age: 10 },
-  ]
+  ];
+  let api = ['1','2','3']
   return (
     <>
       <h1>Greetings</h1>
       <Hello name="arni" agf="34567"/>
       <Hello name="hmann" agf={age}/>
       <Hello name="flix" agf={456 + 123}/>
-      <Hello name="calaa" agf={x}/>
+      <Hello name={name} agf={x}/>
       <Hello name={friends[0].name} agf={friends[0].age}/>
+      <Hello name={friends[1].name} agf={friends[1].age}/>
+      <Hello name={api[0]} agf={api}/>
     </>
   )
 }
