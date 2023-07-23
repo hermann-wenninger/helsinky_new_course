432f08c651bdb79bc95c1f835c500df106998309
hermann wenninger <info@wennhmann.de>
2023-07-23 15:12:12 +0200

    caches

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -1,20 +1,23 @@
 const Hello = (props) => {
-  console.log('fuck the army');
+  console.log('fuck the army', props);
   return (
     <div>
-      <p>hello world and hello to the universe from{props.name}</p>
+      <p>hello world and hello to the universe from{props.name}  with {props.agf}</p>
     </div>
   )
 }
 
 const App = () => {
+  const name = 'Peter'
+  const age = 10
+  let x = 999999
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="arni"/>
-      <Hello name="hmann"/>
-      <Hello name="flix"/>
-      <Hello name="calaa"/>
+      <Hello name="arni" agf="34567"/>
+      <Hello name="hmann" agf={age}/>
+      <Hello name="flix" agf={456 + 123}/>
+      <Hello name="calaa" agf={x}/>
     </div>
   )
 }
