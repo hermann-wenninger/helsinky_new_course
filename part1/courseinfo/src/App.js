fc96d57ad8a1ff276ca3c048adcec3bfec3a044f
hermann wenninger <info@wennhmann.de>
2023-07-24 17:09:26 +0200

    end of courseinfo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,78 +1,68 @@
-const Content = () => {
+const Header = (props) => {
+  console.log('header',props)
   return (
     <div>
-      <Part />
-      <Part />
-      <Part />
+      <h1>{props.coux}</h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  console.log('part',props)
+  return (
+    <div>
+     
+       <h2> {props.part} {props.exercises}</h2>
+     
     </div>
   )
 }
 
-const Part = () => {
-  console.log('part')
+const Content = (props) => {
+  console.log('content',props)
   return (
     <div>
-      <p>
-        Content of Part
-      </p>
+    <Part part={props.parts[0].name} exercises={props.parts[0].exercises}/>
+    <Part part={props.parts[1].name} exercises={props.parts[1].exercises}/>
+    <Part part={props.parts[2].name} exercises={props.parts[2].exercises}/>
     </div>
-  )
-}
+  );
+};
 
-const Total = () => {
-  console.log('total')
+const Total = (props) => {
+  console.log('total',props)
   return (
     <div>
-      <p>
-        Total
-      </p>
+      <p>Number of exercises <strong>{props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</strong></p>
     </div>
-  )
-}
-const Header = () => {
-  console.log('header')
-  return (
-    <div>
-      <p>
-        Total Header
-      </p>
-    </div>
-  )
-}
-
+  );
+};
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-
-      <Header />
-      <Content />
-      <Total />
-
-
-
+      <Header coux={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
+  );
+};
 
-    
-  )
-}
-
-export default App
+export default App;
