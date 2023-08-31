103bb12815627974f09a6933cbbbed499116055d
hermann wenninger <info@wennhmann.de>
2023-08-31 09:06:54 +0200

    test of a golangprogramm

--- part99_go/main.go
+++ part99_go/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
 
 // album represents data about a record album.
 type album struct {
@@ -23,6 +39,7 @@ var albums = []album{
 func getAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, albums)
 }
+
 func main() {
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
