31c38eb25a82fafe0a84bc8c1cbf539cac71a091
hermann wenninger <info@wennhmann.de>
2023-09-11 16:39:52 +0200

    part3 post in rest with simple id

--- part3/index.js
+++ part3/index.js
@@ -29,18 +29,36 @@ app.get('/api/notes', (req, res) => {
   res.json(notes)
 })
 
-app.post('/api/notes', (request, response) => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => n.id)) 
-    : 0
 
-  const note = request.body
-  note.id = maxId + 1
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  }
 
   notes = notes.concat(note)
-  console.log(notes)
+  console.log(note)
   response.json(note)
 })
+
+
 
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
