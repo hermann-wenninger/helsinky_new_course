c7fe98ecb1200b75acd57caea67be7641c414790
hermann wenninger <info@wennhmann.de>
2023-10-16 12:55:36 +0200

    part 2 notes with high ord func > map

--- part2/part2-notes-frontend-part2-1/src/App.jsx
+++ part2/part2-notes-frontend-part2-1/src/App.jsx
@@ -1,16 +1,12 @@
-import Note from './components/Note'
+const App = (props) => {
+  const { notes } = props
 
-const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notes.map(note => <li>{note.content}</li>)}
       </ul>
     </div>
   )
 }
-
-export default App
