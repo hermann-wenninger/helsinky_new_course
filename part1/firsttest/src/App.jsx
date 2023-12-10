0e183f160c929228620dc3116b2ec53f1d51eb8a
hermann wenninger <info@wennhmann.de>
2023-12-10 11:56:31 +0100

    wiederholung

--- part1/firsttest/src/App.jsx
+++ part1/firsttest/src/App.jsx
@@ -1,12 +1,29 @@
 
 const App = () => {
 
+  const now = new Date()
+  let a = 100
+  let b = 200
+  console.log(now, a+b)
+
 
   return (
   <>
-  <h1>hallo</h1>
+  <h1> a + b = {a+b}</h1>
+  <Abc />
+  <Hello name='hmann' age='59' />
     </>
   )
 }
 
 export default App
+const Abc = ()=>{
+ return (
+    <div className="xxx">goloria in exzelsior</div>
+  )
+}
+const Hello = (props)=>{
+  return(
+    <h2>Hallo {props.name} you are jears {props.age} old</h2>
+  )
+}
