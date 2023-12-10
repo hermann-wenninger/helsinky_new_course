4e04b2ccc77f19e33035643d2cf4ce93f2f8137b
hermann wenninger <info@wennhmann.de>
2023-12-10 12:21:19 +0100

    props

--- part1/firsttest/src/App.jsx
+++ part1/firsttest/src/App.jsx
@@ -5,25 +5,30 @@ const App = () => {
   let a = 100
   let b = 200
   console.log(now, a+b)
+  let name= 'karl'
+  let age = 23
+  const gloria = 'vollkommen outof range'
 
 
   return (
   <>
   <h1> a + b = {a+b}</h1>
-  <Abc />
+  <Abc gloria={gloria}/>
   <Hello name='hmann' age='59' />
+  <Hello name={name} age={age} />
     </>
   )
 }
 
 export default App
-const Abc = ()=>{
+const Abc = (props)=>{
  return (
-    <div className="xxx">goloria in exzelsior</div>
+    <div className="xxx">goloria von {props.gloria}</div>
   )
 }
 const Hello = (props)=>{
-  return(
+  return([
+    <h5 style={{color:'red', fontFamily:'fantasy'}}>Greetings</h5>,
     <h2>Hallo {props.name} you are jears {props.age} old</h2>
-  )
+  ])
 }
