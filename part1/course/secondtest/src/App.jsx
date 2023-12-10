3ecb630161559ab57cc3643d5dc395074c7edc75
hermann wenninger <info@wennhmann.de>
2023-12-10 14:08:41 +0100

    part1 course2.0

--- part1/course/secondtest/src/App.jsx
+++ part1/course/secondtest/src/App.jsx
@@ -8,11 +8,18 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div style={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+    <div>
       <Header course={course}/>
-  <Content part={part1} exercises={exercises1}/>
-  <Content part={part2} exercises={exercises2}/>
-  <Content part={part3} exercises={exercises3}/>
+  <Content part1={part1} 
+          part2={part2}   
+          part3={part3} 
+          ex1={exercises1}
+          ex2={exercises2}
+          ex3={exercises3}
+
+                
+                />
+
   <Total ex1={exercises1} ex2={exercises2} ex3={exercises3}/>
  </div>
  )
@@ -24,20 +31,28 @@ export default App
 const Header = (props)=>{
   console.log(props)
 return(
-   <h1>{props.course}</h1>)
-   
- 
-}
+   <h1>{props.course}</h1>
+   )}
+
 const Content = (props)=>{
-  console.log(props)
+  
   return(
     <ul>
-      <li><strong>Part:</strong> {props.part} <strong>Exercises:</strong>{props.exercises}</li>
+      <Part part={props.part1} exercises={props.ex1}/>
+      <Part part={props.part2} exercises={props.ex2}/>
+      <Part part={props.part3} exercises={props.ex3}/>
     </ul>
-  )
-}
+  )}
+
 const Total =(props)=>{
   return(
     <p>All Exes: {props.ex1 + props.ex2 + props.ex3}</p>
+  )
+}
+
+const Part =(props)=>{
+  console.log(props)
+  return(
+    <li><strong>Part:</strong> {props.part} <strong>Exercises:</strong>{props.exercises}</li>
   )
 }
